1675c0042d9399d020ad6e3334e038a310764905
ghghghhghg <slepchenkov.boris@mail.ru>
2026-07-08 13:24:32 +0300

    add exam mock with timer

--- school/models.py
+++ school/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils import timezone
+
 from cloudinary.models import CloudinaryField
 
 from django.utils.text import slugify
